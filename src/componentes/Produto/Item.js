961efd6e2b6c60fc3848613e205c95ad1b26d009
douglas <douglasklemes0303@gmail.com>
2019-10-11 16:32:44 -0300

    criar logica de add e remover produtos do carrinho

--- src/componentes/Produto/Item.js
+++ src/componentes/Produto/Item.js
@@ -1,20 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./Item.scss";
 import Button from '../Button/Button';
-import Rey from "../../imagem/1.jpg";
 
-const Item = ({titulo, imagem, descricao}) => {
+
+const Item = ({titulo, imagem, descricao, preco}) => {
+    const [count,setCount] = useState(0)
+
+    const adicionar = () =>{
+        if( count >= 0 && count < 10){
+            setCount( count + 1)
+        }
+    }
+
+    const remover = () =>{
+        if(count !== 0 ){
+            setCount(count - 1)
+        }
+    }
     return (
         <div className="Item">
-            <h2>Rey</h2>
-            <img src={Rey} alt="item"/>
+            <h2>{titulo}</h2>
+            <img src={imagem} alt="item"/>
             <p>{descricao}</p>
+            <p>R${preco}</p>
+            <p>{count}</p>
             <div>
-            <Button>-</Button>
-            <Button>+</Button>
+            <Button funcao={remover}>-</Button>
+            <Button funcao={adicionar}>+</Button>
 
             </div>
-    
+            <Button>Adicionar ao carrinho</Button>
 
         </div>
     )
--- src/componentes/Produto/Produto.js
+++ src/componentes/Produto/Produto.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import "./Produto.scss";
 import Item from "./Item";
+import Rey from "../../imagem/1.jpg";
+import Solo from "../../imagem/2.jpg"
+import Button from '../Button/Button';
 
 const Produto = () => {
+    // const [produto1, setProduto1 ] = useState({})
+    // const batata = () => {
+    //     setProdutos({nome: "x", price: "100", qnt: 12})
+    // }
     return (
-        <div className="produto">
-            <Item />
+        <div className="Produto">
+        <Item titulo="Rey" imagem={Rey} descricao="Boneco star wars ep VII" preco="300,00"/>
+        <Item titulo="Han Solo" imagem={Solo} descricao="Boneco star wars ep IV" preco="200,00"/>
+        <Item titulo="Rey" imagem={Rey} descricao="Boneco star wars ep VII" preco="300,00"/>
+        <Item titulo="Rey" imagem={Rey} descricao="Boneco star wars ep VII" preco="300,00"/>
+        <Item titulo="Rey" imagem={Rey} descricao="Boneco star wars ep VII" preco="300,00"/>
+        <Item titulo="Rey" imagem={Rey} descricao="Boneco star wars ep VII" preco="300,00"/>
+        <div>
+        <Button id="botao">Finalizar Compra</Button>
+        </div>
+
         </div>
     )
 }
